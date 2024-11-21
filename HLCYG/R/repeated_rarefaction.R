d8633a84b560354819ad5a87903f66477c403077
MagPld <polder.magdalena@gmail.com>
2024-11-21 13:35:20 +0100

    added comments

---
+++ HLCYG/R/repeated_rarefaction.R
@@ -0,0 +1,105 @@
+#' Perform one iteration of repeated rarefaction and produce ordination plot
+#'
+#' @param physeq A phyloseq object.
+#' @param repeats A positive integer. Indicates the amount of repeats run. A value = 1 means no repeats are used.
+#' @param threshold A positive integer. The threshold value for the rarefaction.
+#' @param method A string. Currently only "NMDS" supported.
+#' @param sample_id A string. Name of the column with sample IDs.
+#' @param colorb A string. Name of the column with data to colour the graph by.
+#' @param shapeb A string. Name of the column with data to shape the points on the graph by.
+#' @param cloud Boolean. Aesthetic setting for the graph. Default is FALSE. TRUE shows datapoints for all repeats.
+#' @param ellipse Boolean. Aesthetic setting for the graph. Default is TRUE.
+#' @returns A list with repeat count table, repeat info table, ordination object, physeq object, dataframe with all repeat ordination positions, dataframe with median ordination positions, and ordination plot.
+#' @examples
+#' repeated_rarefaction(HLCYG_physeq_data, 5, 500, "NMDS", "sample_id", "location", "type", T, F)
+#' repeated_rarefaction(HLCYG_physeq_data, 10, 250, "NMDS", "sample_id", "location", "type", T, T)
+repeated_rarefaction <- function(physeq, repeats, threshold, method, sample_id, colorb, shapeb, cloud = FALSE, ellipse = TRUE) {
+  step1 <- rep_raref(as(otu_table(physeq), "matrix"), sample_data(physeq), threshold, repeats)
+  step2 <- ord_and_mean(step1$repeat_count, step1$repeat_info, sample_data(physeq), repeats, method, sample_id)
+  step3 <- plot_rep_raref(step2$ordinate_object, step2$physeq_object, step2$df_all, step2$df_median, colorb, shapeb, cloud, ellipse)
+
+  return(list("repeat_count" = step1$repeat_count, "repeat_info" = step1$repeat_info, "ordinate_object" = step2$ordinate_object, "physeq_object" = step2$physeq_object, "df_all" = step2$df_all, "df_median" = step2$df_median, "plot" = step3))
+}
+
+rep_raref <- function(count, info, threshold, repeats) {
+  if (repeats == 0) {
+    warning("repeats can't be 0. It needs to be a positive integer. Performs rarefaction without repetition.")
+  }
+
+  if (repeats < 0) {
+    warning("repeats can't be negative. It needs to be a positive integer. Performs rarefaction without repetition.")
+  }
+
+  # Set up working files and perform one initial rarefaction
+  rarified_count <- rrarefy(t(count), threshold)
+  duplicated_info <- info
+  sample_names_list1 <- row.names(info)
+  sample_names_list2 <- colnames(count)
+
+  # Perform repeated rarefaction
+  # If repeats = 1, skips loop as no repetitions are needed.
+  if (repeats > 1) {
+    for (x in 2:repeats) {
+      rarified_count <- rbind(rarified_count, rrarefy(t(count), threshold))
+      duplicated_info <- rbind(duplicated_info, info)
+      sample_names_list1 <- append(sample_names_list1, paste0(row.names(info), paste0("_", x)))
+      sample_names_list2 <- append(sample_names_list2, paste0(colnames(count), paste0("_", x)))
+    }
+  }
+
+  rarified_count <- decostand(rarified_count, "normalize")
+
+  # Transform both count data and info into dataframes
+  # Corrects row names for compatibility in ordination function.
+  duplicated_info <- as.data.frame(duplicated_info)
+  row.names(duplicated_info) <- sample_names_list1
+  rarified_count <- as.data.frame(rarified_count)
+  row.names(rarified_count) <- sample_names_list2
+
+  return(list("repeat_count" = rarified_count, "repeat_info" = duplicated_info))
+}
+
+ord_and_mean <- function(repeat_count, repeat_info, sample_info, repeats, method, sample_id) {
+  if (!(sample_id %in% colnames(repeat_info))) {
+    stop(paste0(sample_id, " is not a column name in input for repeat_info."))
+  }
+
+  # Convert the input into physeq objects
+  rare_count_phy_repeat <- otu_table(t(repeat_count), taxa_are_rows = T)
+  sample_info_tab_phy_repeat <- sample_data(repeat_info)
+  rare_physeq_repeat <- phyloseq(rare_count_phy_repeat, sample_info_tab_phy_repeat)
+
+  # Perform the Ordination calculation
+  vst_pcoa <- ordinate(rare_physeq_repeat, method = method, distance = "bray")
+
+  # Convert ordination result into a data frame object
+  my_plot <- (plot_ordination(rare_physeq_repeat, vst_pcoa, justDF = TRUE))
+
+  # Create matrix with median position for each sample
+  group_by_sample <- my_plot %>%
+    group_by(sample_id) %>%
+    summarise(
+      NMDS1 = median(NMDS1),
+      NMDS2 = median(NMDS2)
+    )
+
+  df_median <- merge(group_by_sample, data.frame(sample_info), sample_id)
+
+  return(list("ordinate_object" = vst_pcoa, "physeq_object" = rare_physeq_repeat, "df_all" = my_plot, "df_median" = df_median))
+}
+
+plot_rep_raref <- function(ordination, physeq, all_positions, median_positions, color, shape, cloud, ellipse) {
+  color_median <- unlist(median_positions[color])
+  color_tot <- unlist(all_positions[color])
+
+  shape_median <- unlist(median_positions[shape])
+  shape_tot <- unlist(all_positions[shape])
+
+  # Create the plot and print it
+  p <- plot_ordination(physeq, ordination, color = color, shape = shape)
+  if (!cloud) { p$layers <- p$layers[-1] }
+  p <- p + {if (ellipse) stat_ellipse(linetype = 1, lwd = 0.8, aes(color = color_tot, group = all_positions$sample_id))} +
+    {if (!cloud) geom_point(data = median_positions, mapping = aes(x = median_positions[, 2], y = median_positions[, 3], color = color_median, shape = shape_median))}
+
+  return(p)
+}
---
+++ HLCYG/R/threshold_testing.R
@@ -0,0 +1,67 @@
+#' Test a range of threshold values for repeated rarefaction.
+#'
+#' @param physeq A phyloseq object.
+#' @param repeats A positive integer. Indicates the amount of repeats run. A value = 1 means no repeats are used.
+#' @param t_min An integer. Minimum value for the threshold testing range.
+#' @param t_max An integer. Maximum value for the threshold testing range.
+#' @param t_step An float. Step value for the threshold testing range. A value between 0 and 1 will cause the same threshold to be tested more than once.
+#' @param method A string. Currently only "NMDS" supported.
+#' @param sample_id A string. Name of the column with sample IDs.
+#' @param groupb A string. Name of the column with data to group the points by.
+#' @returns A list with repeat count table, repeat info table, ordination object, physeq object, dataframe with all repeat ordination positions, dataframe with median ordination positions, and ordination plot.
+#' @examples
+
+
+test_threshold <- function(physeq, repeats = 10, t_min = 5, t_max = 250, t_step = 1, method = "NMDS", sample_id, groupb) {
+  thresholds <- as.integer(seq.int(t_min, t_max, by = t_step))
+  index_data <- matrix(nrow = 0, ncol = 3)
+  colnames(index_data) <- c("Repeat_Amount", "Threshold", "Index")
+
+  # Loops through all thresholds set in the parametteser above
+  for (y in repeats) {
+    for (x in thresholds) {
+      print(paste("Running with ", y, " repeats and ", x, " threshold"))
+      index <- get_index(physeq, sample_data(physeq), repeats= y, threshold = x, method, sample_id, groupb)
+      index_data <- rbind(index_data, c(toString(y), x, index))
+    }
+  }
+
+
+  # colnames(index_data) <- c("Repeat_Amount", "Threshold", "Index")
+  index_data <- as.data.frame(index_data)
+  index_data$Threshold <- as.numeric(index_data$Threshold)
+  index_data$Index <- as.numeric(index_data$Index)
+
+  print(index_data)
+
+  ggplot(index_data, aes(x = Threshold, y = Index, color = Repeat_Amount)) +
+    geom_point() +
+    labs(x = "Rarefaction Threshold", y = "Calinski-Harabasz pseudo F-statistic") +
+    geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = F)
+}
+
+get_index <- function(physeq, sample_info, repeats, threshold, method, sample_id, groupb) {
+  # Calls the first two steps of the repeated rarefaction methods
+  step1 <- rep_raref(as(otu_table(physeq), "matrix"), sample_data(physeq), threshold, repeats)
+  step2 <- ord_and_mean(step1$repeat_count, step1$repeat_info, sample_info, repeats, method, sample_id)
+
+  # Extract just position data and create a list of "true" clusters
+  just_positions <- as.data.frame(step2$df_median[,2:3])
+  cluster_true_pre <- unlist(step2$df_median[groupb])
+  cluster_names <- list()
+  cluster_true <- cluster_true_pre
+
+  # Index function requires clusters to be numbered.
+  # Therefore whatever the cluster names are called, they are here converted to numbers
+  existing_groups <- unique(cluster_true)
+  for (x in 1:length(cluster_true_pre)) {
+    cluster_true[x] <- which(existing_groups == cluster_true_pre[x])
+  }
+
+  # Calculate index
+  # It's possible to change which index is used
+  # Currently it's set to G1
+  cluster_true <- as.numeric(unlist(cluster_true))
+
+  return(clusterSim::index.G1(just_positions, cluster_true))
+}
