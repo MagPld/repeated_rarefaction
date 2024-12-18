434e71965c98440f7b41f3fa0442c9fb5f8f8231
MagPld <polder.magdalena@gmail.com>
2024-12-18 14:37:10 +0100

    added cores

--- HLCYG/R/repeated_rarefaction_3.R
+++ HLCYG/R/repeated_rarefaction_3.R
@@ -2,29 +2,28 @@
 #'
 #' @param physeq A phyloseq object.
 #' @param repeats A positive integer. Indicates the amount of repeats run.
-#' A value = 1 means no repeats are used.
-#' @param threshold A positive integer. The threshold value for the rarefaction.
-#' @param method A string. Currently only "NMDS" supported.
-#' @param sample_id A string. Name of the column with sample IDs.
+#' A value = 1 means no repeats are used. Default = 50.
+#' @param threshold A positive integer. The threshold value for the rarefaction. Default = 250
 #' @param colorb A string. Name of the column with data to colour the graph by.
 #' @param shapeb A string. Name of the column with data to shape the points on the graph by.
 #' @param cloud Boolean. Aesthetic setting for the graph. Default is FALSE.
 #' TRUE shows datapoints for all repeats.
 #' @param ellipse Boolean. Aesthetic setting for the graph. Default is TRUE.
+#' @param cores An int. The amount of cores used when running. The default = 4.
 #' @returns A list with repeat count table, repeat info table, ordination object,
 #' physeq object, dataframe with all repeat ordination positions, dataframe with median ordination positions, and ordination plot.
 #' @examples
 #' repeated_rarefaction(HLCYG_physeq_data, repeats=5, threshold=500, method="NMDS", colorb="sample_id", shapeb="location", T, F)
 #' repeated_rarefaction(HLCYG_physeq_data, repeats=10, threshold=250, method="NMDS", colorb="sample_id", shapeb="location", T, T)
-repeated_rarefaction_2 <- function(physeq, repeats = 50, threshold = 250, colorb="sample_id", group="sample_id", cloud = TRUE, ellipse = FALSE) {
-  
+repeated_rarefaction_2 <- function(physeq, repeats = 50, threshold = 250, colorb="sample_id", group="sample_id", cloud = TRUE, ellipse = FALSE, cores = 4) {
+
   # Make the rownames of the Phyloseq object a new "sample_id" variable for the sample data.
   # (this covers the case in which no sample_id column is present in the sample data)
   # Then set it to a separate variable.
   sample_data(physeq)$sample_id <- rownames(sample_data(physeq))
-  
+
   # ============ Checks and warnings
-  
+
   if (!(colorb %in% names(sample_data(physeq)))) {
     stop(paste("'",colorb,"' is not a column name in the sample information in the inputed phyloseq object.
                   repeated_rarefaction needs an existing column to color the ordination plot by.", sep=""))
@@ -50,14 +49,14 @@ repeated_rarefaction_2 <- function(physeq, repeats = 50, threshold = 250, colorb
     warning("Too few repeats to draw confidence ellipses.")
     ellipse <- F
   }
-  
-  # Perform the different steps of the repeated rarefaction algorithm 
+
+  # Perform the different steps of the repeated rarefaction algorithm
   step1 <- rep_raref_2(data.frame(t(otu_table(physeq))), threshold, repeats)
-  step2 <- ord_and_mean_2(step1$rarefied_matrix_list, repeats)
+  step2 <- ord_and_mean_2(step1$rarefied_matrix_list, repeats, cores)
   step3 <- plot_rep_raref_2(step2$aligned_ordinations, step2$consensus_coordinates, sample_data(physeq), colorb, group, cloud, ellipse, "Aligned Ordinations with Consensus Overlaid")
 
   print(step3$plot)
-  
+
   return(invisible(list("repeats" = repeats, "df_consensus_coordinates" = step3$consensus_df, "df_all" = step3$df_all, "plot" = step3$plot)))
 }
 
@@ -96,62 +95,58 @@ rep_raref_2 <- function(count, threshold, repeats) {
 
 #' Calculates ordination on the repeated count input and as well as a median result for each original datapoint.
 #'
-#' @param repeat_count A matrix. Contains the repeated count data.
-#' @param repeat_info A matrix. Contains the repeated data info.
-#' @param sample_info Sample Data object from the phyloseq package. Contains data info without repetitions.
+#' @param rarified_matrix_list A matrix. Contains the repeated count data.
 #' @param repeats An integer. The amount of repeats that has been performed on the data.
-#' @param method A string. Currently only "NMDS" supported.
-#' @param sample_id A string. Name of the column with sample IDs.
 #' @returns Returns a list containg an ordination object, a phyloseq object with
 #' the repeated count data, a dataframe with all positions from the ordination
 #' calculaction, and a datafram with just the median position from the calculation.
-ord_and_mean_2 <- function(rarefied_matrix_list, repeats) {
-  
+ord_and_mean_2 <- function(rarefied_matrix_list, repeats, cores = 4) {
+
   #========================= ordinations and plots generation
-  
+
   # Initialize a list to store ordinations
   ordinations <- list()
-  
+
   # Set up parallel backend to use available cores
-  numCores <- detectCores() - 1  # Use one less than the total number of cores
-  cl <- makeCluster(numCores)
+  # numCores <- detectCores() - 1  , Use one less than the total number of cores
+  cl <- makeCluster(cores)
   registerDoParallel(cl)
-  
+
   # Perform parallel computation using foreach
   results <- foreach(i = 1:length(rarefied_matrix_list), .packages = c('vegan', 'ggplot2')) %dopar% {
     # Calculate Bray-Curtis distance
     dist_matrix <- vegdist(rarefied_matrix_list[[i]], method = "bray")
-    
+
     # Perform PCoA (Principal Coordinates Analysis)
     ordination <- cmdscale(dist_matrix, k = 2)
-    
+
     # Convert ordination result to data frame
     ord_df <- as.data.frame(ordination)
     colnames(ord_df) <- c("PCoA1", "PCoA2")
     ord_df$Sample <- rownames(ordination)
-    
+
     # Return a list containing the ordination and the plot
     list(ordination = ordination)
-    
+
   }
-  
+
   # Stop the cluster after computation
   stopCluster(cl)
-  
-  
+
+
   # Extract ordinations and plots from the results
   for (i in 1:length(results)) {
     ordinations[[i]] <- results[[i]]$ordination
   }
-  
+
   #================================ procrustes
-  
+
   # Perform Procrustes analysis to align all ordinations to the first one
   aligned_ordinations <- lapply(ordinations, function(x) procrustes(ordinations[[1]], x)$Yrot)
-  
+
   # Convert list to array for consensus calculation
   aligned_array <- array(unlist(aligned_ordinations), dim = c(nrow(aligned_ordinations[[1]]), ncol(aligned_ordinations[[1]]), length(aligned_ordinations)))
-  
+
   # Compute consensus using mean shape
   consensus_coords <- mshape(aligned_array)
 
@@ -172,10 +167,10 @@ ord_and_mean_2 <- function(rarefied_matrix_list, repeats) {
 #' #' @param ellipse Boolean. Aesthetic setting for the graph. Default is TRUE.
 #' @returns Returns an ordination plot.
 plot_rep_raref_2 <- function(aligned_ordinations, consensus_coordinates, info, color, group, cloud, ellipse, title) {
-  
+
   # Combine aligned ordinations into one data frame for plotting
   aligned_df <- data.frame()
-  
+
   for (i in 1:length(aligned_ordinations)) {
     temp_df <- as.data.frame(aligned_ordinations[[i]])
     colnames(temp_df) <- c("Dim1", "Dim2")
@@ -185,71 +180,72 @@ plot_rep_raref_2 <- function(aligned_ordinations, consensus_coordinates, info, c
     temp_df[[group]] <- info[[group]]
     aligned_df <- rbind(aligned_df, temp_df)
   }
-  
+
   # Convert consensus_coordinates to a data frame
   consensus_df <- as.data.frame(consensus_coordinates)
   colnames(consensus_df) <- c("Dim1", "Dim2")
   consensus_df$sample_id <- rownames(aligned_ordinations[[1]])
-  
+
   # Determine how many colors are needed to plot the levels of the color variable
   num_levels <- length(unique(consensus_df[[color]]))
-  
+
   plot <- ggplot()
-  
-  if (cloud) {
+
+  if (num_levels <= 6) {
+    # Use variable-based coloring if there are 6 or fewer categories
+    plot <- plot +
+      geom_point(
+        data = aligned_df,
+        aes(x = Dim1, y = Dim2, color = .data[[color]]),
+        alpha = 0.3
+      )
+  } else {
+    # Use a fixed grey color for all points if there are more than 6 categories
+    plot <- plot +
+      geom_point(
+        data = aligned_df,
+        aes(x = Dim1, y = Dim2),
+        color = "grey70",
+        alpha = 0.3
+      )
+    }
+
+  if (!cloud) {
+    plot$layers <- plot$layers[-1]
+  }
+
+
+  if (ellipse) {
     if (num_levels <= 6) {
-      # Use variable-based coloring if there are 6 or fewer categories
+      # Ellipses colored by the color variable
       plot <- plot +
-        geom_point(
+        stat_ellipse(
           data = aligned_df,
-          aes(x = Dim1, y = Dim2, color = .data[[color]]),
-          alpha = 0.3
+          aes(x = Dim1, y = Dim2, color = .data[[color]], group = .data[[group]]),
+          linetype = 1, lwd = 0.8
         )
     } else {
-      # Use a fixed grey color for all points if there are more than 6 categories
+      # Ellipses in grey if more than 6 categories
+      # Still grouping by the color variable to get multiple ellipses if there are multiple groups
       plot <- plot +
-        geom_point(
+        stat_ellipse(
           data = aligned_df,
-          aes(x = Dim1, y = Dim2),
+          aes(x = Dim1, y = Dim2, group = .data[[group]]),
           color = "grey70",
-          alpha = 0.3
+          linetype = 1, lwd = 0.8
         )
     }
-    
-    # If ellipse also = TRUE (you cannot draw ellipse if cloud = FALSE)
-    if (ellipse) {
-      if (num_levels <= 6) {
-        # Ellipses colored by the color variable
-        plot <- plot +
-          stat_ellipse(
-            data = aligned_df,
-            aes(x = Dim1, y = Dim2, color = .data[[color]], group = .data[[group]]),
-            linetype = 1, lwd = 0.8
-          )
-      } else {
-        # Ellipses in grey if more than 6 categories
-        # Still grouping by the color variable to get multiple ellipses if there are multiple groups
-        plot <- plot +
-          stat_ellipse(
-            data = aligned_df,
-            aes(x = Dim1, y = Dim2, group = .data[[group]]),
-            color = "grey70",
-            linetype = 1, lwd = 0.8
-          )
-      }
-    }
-    
   }
-  
+
   # Plot all aligned ordinations (consensus points)
   plot <- plot +
     geom_point(data = consensus_df, aes(x = Dim1, y = Dim2), color = "red", size = 2) +
     theme_minimal() +
-    theme(plot.title = element_text(hjust = 0.5)) + 
+    theme(plot.title = element_text(hjust = 0.5)) +
     ggtitle(title) +
     xlab("Dimension 1") +
     ylab("Dimension 2")
-  
+
   return(invisible(list("plot" = plot, "consensus_df" = consensus_df, "df_all" = aligned_df)))
 }
 
--- HLCYG/R/threshold_testing_3.R
+++ HLCYG/R/threshold_testing_3.R
@@ -7,10 +7,11 @@
 #' @param t_step An float. Step value for the threshold testing range. A value between 0 and 1 will cause the same threshold to be tested more than once.
 #' @param sample_id A string. Name of the column with sample IDs.
 #' @param group A string. Name of the column with data to group the points by.
+#' @param cores An int. The amount of cores used when running. The default = 4.
 #' @returns A graph with rarefaction threshold as the x axis, and the y axis is the calculated Calinski-Harabasz F-statistic. Each dot indicates the clustering score for the repeated ordination on a single threshold value. The higher score the better.
 #' @examples
 #' test_threshold(HLCYG_physeq_data, repeats = 10, t_min = 200, t_max =1500, t_step = 5, group="location")
-test_threshold <- function(input, repeats = 10, t_min = 50, t_max = 250, t_step = 1, group = "sample_id") {
+test_threshold <- function(input, repeats = 10, t_min = 50, t_max = 250, t_step = 1, group = "sample_id", cores = 4) {
   # Check if input is a Phyloseq object or a file path
   if (inherits(input, "phyloseq")) {
     physeq <- input
@@ -20,107 +21,107 @@ test_threshold <- function(input, repeats = 10, t_min = 50, t_max = 250, t_step 
   } else {
     stop("Input must be a Phyloseq object or a file path to an OTU count table (.csv or .tsv).")
   }
-  
+
   # Make the rownames of the Phyloseq object a new "sample_id" variable for the sample data.
   # (this covers the case in which no sample_id column is present in the sample data)
   # Then set it to a separate variable.
   sample_data(physeq)$sample_id <- rownames(sample_data(physeq))
-  
+
   thresholds <- seq.int(t_min, t_max, by = t_step)
-  
+
   # Store ordination points and centroids
   ordination_points <- list()
   consensus_coordinates <- list()
-  
+
   for (y in repeats) {
     for (x in thresholds) {
       message(paste("Running with", y, "repeats and", x, "threshold"))
-      
+
       step1 <- rep_raref_2(data.frame(t(otu_table(physeq))), threshold = x, repeats = y)
       step2 <- ord_and_mean_2(step1$rarefied_matrix_list, repeats)
-      
-      # step2 should return something like: 
+
+      # step2 should return something like:
       # $aligned_ordinations: A list of ordination coordinates for each repeat
       # $consensus_coordinates: a matrix or df of centroid coords
-      
+
       # Store aligned ordination points for this threshold
-      # Note: They may not actually be "aligned" in any external sense now, 
+      # Note: They may not actually be "aligned" in any external sense now,
       # but are just coordinates from each repetition's ordination.
       ordination_points[[paste0("threshold_", x)]] <- step2$aligned_ordinations
-      
+
       # Store consensus coordinates for this threshold
       consensus_coords <- step2$consensus_coordinates
       colnames(consensus_coords) <- c("Dim1", "Dim2")
       consensus_coordinates[[paste0("threshold_", x)]] <- consensus_coords
     }
   }
-  
+
   # Flatten the list of lists containing individual repeat attempts for each threshold
   # After flattening, each element in flattened_list corresponds to a single threshold and
   # is a combined data frame (or matrix) of coordinates from all repeats for that threshold.
   flattened_list <- lapply(ordination_points, function(sublist) {
     do.call(rbind, sublist)
   })
-  
+
   # =========================
   # Remove Procrustes alignment
   # Instead, we will normalize all coordinates to unit variance.
-  
+
   # Combine all points across all thresholds to compute global scaling parameters
   all_points <- do.call(rbind, flattened_list)
-  
+
   # Compute global means and sds for each axis
   axis_means <- colMeans(all_points, na.rm = TRUE)
   axis_sds   <- apply(all_points, 2, sd, na.rm = TRUE)
-  
+
   # Define a normalization function
   normalize_coords <- function(mat, means, sds) {
     sweep(sweep(mat, 2, means, "-"), 2, sds, "/")
   }
-  
+
   # Normalize each threshold's ordination points
   scaled_points <- lapply(flattened_list, function(x) normalize_coords(x, axis_means, axis_sds))
-  
+
   # Determine global axis limits after normalization (with some tolerance)
   all_scaled_points <- do.call(rbind, scaled_points)
   x_limits <- range(all_scaled_points[, 1], na.rm = TRUE)
   y_limits <- range(all_scaled_points[, 2], na.rm = TRUE)
   x_limits <- c(x_limits[1] - 0.2, x_limits[2] + 0.2)
   y_limits <- c(y_limits[1] - 0.2, y_limits[2] + 0.2)
-  
+
   # =====================
   # Plotting and Index Calculation
-  
+
   plots <- list()
   index_data <- matrix(nrow = 0, ncol = 3)
-  
+
   for (y in repeats) {
     for (x in thresholds) {
-      
+
       # Retrieve normalized data for this threshold
       # scaled_points[[paste0("threshold_", x)]] is a combined matrix with all repeats
       # We need to split by repeats again
       current_key <- paste0("threshold_", x)
       df <- as.data.frame(scaled_points[[current_key]])
-      
+
       # Split into repeats
       n_points <- nrow(df)
       # each threshold run had 'repeats' ordinations, so we split equally
       split_reps <- split(df, ceiling(seq_len(n_points) / (n_points/y)))
-      
+
       # Plot
       # 'consensus_coordinates' should also be normalized.
       norm_consensus <- normalize_coords(consensus_coordinates[[current_key]], axis_means, axis_sds)
-      
+
       step3 <- plot_rep_raref_2(split_reps, norm_consensus, sample_data(physeq), color = group, group = group, cloud = TRUE, ellipse = TRUE, title = paste0("Threshold: ", x))
       plot <- step3$plot
       plots[[current_key]] <- plot + xlim(x_limits) + ylim(y_limits)
-      
+
       # ======================== Index calculation
-      
+
       # aligned_df <- data.frame()
       info <- sample_data(physeq)
-      
+
       # Fix the normalized consensus coordinates adding group variable
       norm_consensus_df <- data.frame
       temp_df <- as.data.frame(norm_consensus)
@@ -128,34 +129,34 @@ test_threshold <- function(input, repeats = 10, t_min = 50, t_max = 250, t_step 
       rownames(temp_df) <- rownames(split_reps[[1]])
       temp_df[[group]] <- info[[group]]
       norm_consensus_df <- rbind(temp_df)
-      
+
       # Extract just position data for the individual points
       just_positions_consensus <- norm_consensus_df[,1:2]
-      
+
       # Determine clusters according to the "group" variable
       clusters_consensus <- unlist(norm_consensus_df[[group]])
       existing_groups_consensus <- unique(clusters_consensus)
-      
+
       # Convert cluster labels to numbers
       cluster_consensus_numbers <- match(clusters_consensus, existing_groups_consensus)
-      
+
       # Calculate index
       index <- clusterSim::index.G1(just_positions_consensus, cluster_consensus_numbers)
 
       index_data <- rbind(index_data, c(toString(y), x, index))
     }
   }
-  
+
   colnames(index_data) <- c("Repeat_Amount", "Threshold", "Index")
   index_data <- as.data.frame(index_data)
   index_data$Threshold <- as.numeric(index_data$Threshold)
   index_data$Index <- as.numeric(index_data$Index)
-  
+
   # Plot the indexes
   index_plot <- ggplot(index_data, aes(x = Threshold, y = Index, color = Repeat_Amount)) +
     geom_point() +
     labs(x = "Rarefaction Threshold", y = "Calinski-Harabasz pseudo F-statistic") +
     geom_smooth()
-  
+
   return(list(index_plot = index_plot, ordination_plots = plots))
 }
